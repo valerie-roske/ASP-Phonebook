f1b4b34044810c342e575ee59212d4a95b501153
Michael Lennon <mlennon3@gmail.com>
2014-06-23 13:55:38 -0500

    removed unneeded files

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Phonebook
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Contacts", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
