5a0fdde9cbe4f76ff6ac63956518c8d640bd142a
Michael Lennon <mlennon3@gmail.com>
2014-06-23 16:18:08 -0500

    Unmapped pages redirect to a custom error page

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phonebook.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;  //you may want to set this to 200
+            return View("404");
+        }
+
+    }
+}
