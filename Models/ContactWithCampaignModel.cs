deaf701bc779c9a1203595f9ce65b26d69a14be9
Anirudh <rudysback@gmail.com>
2014-06-26 12:51:26 -0500

    [Valerie & Anirudh] Export All Campaigns in a Date Range

---
+++ Models/ContactWithCampaignModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Phonebook.Entities;
+
+namespace Phonebook.Models
+{
+    public class ContactWithCampaign
+    {
+        public Contact Contact { get; set; }
+        public Campaign Campaign { get; set; }
+    }
+}
