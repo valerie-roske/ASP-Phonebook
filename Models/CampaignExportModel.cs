4fae9f38c1e87d39dd0042a1473c5d9949ead141
Anirudh <rudysback@gmail.com>
2014-06-26 13:57:16 -0500

    valerie & anirudh: updated text on export form

--- Models/CampaignExportModel.cs
+++ Models/CampaignExportModel.cs
@@ -6,8 +6,10 @@ namespace Phonebook.Models
     public class CampaignExportModel
     {
         [DataType(DataType.Date)]
+        [Display(Name = "Start Date")]
         public DateTime StartDateTime { get; set; }
         [DataType(DataType.Date)]
+        [Display(Name = "End Date")]
         public DateTime EndDateTime { get; set; }
     }
 }
